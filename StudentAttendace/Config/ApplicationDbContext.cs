91453d3b3cb7a0e07f7c7c454fefbdf6d19f2ed9
ElMarto12 <m10danys@gmail.com>
2024-09-09 14:41:48 +0300

    Db Setup

---
+++ StudentAttendace/Config/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAttendace.Models.DbModels;
+
+namespace StudentAttendace.Config;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext
+{
+    public required DbSet<User> Users { get; set; }
+}
---
+++ StudentAttendace/Models/DbModels/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentAttendace.Models.DbModels;
+
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int UserID { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    [Column(TypeName = "varchar(65)")]
+    public string Email { get; set; }
+    
+    [Required]
+    [PasswordPropertyText]
+    [Column(TypeName = "varchar(255)")]
+    public string Password { get; set; }
+    
+    [Required]
+    [Column(TypeName = "varchar(65)")]
+    public string Role { get; set; }
+}
