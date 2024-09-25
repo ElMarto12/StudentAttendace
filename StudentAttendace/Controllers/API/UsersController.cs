8d178fd22867d52f7291e0215126af0eda4e14a8
ElMarto12 <m10danys@gmail.com>
2024-09-25 16:31:01 +0300

    Updated Deritives

--- StudentAttendace/Controllers/API/UsersController.cs
+++ StudentAttendace/Controllers/API/UsersController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using StudentAttendace.Config;
 using StudentAttendace.Models.DbModels;
 
