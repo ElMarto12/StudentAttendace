cf26002fdcecde6b96de79b9db444a5f8972ef03
ElMarto12 <m10danys@gmail.com>
2024-10-15 23:42:05 +0300

    Added separate service class - GroupService.cs

---
+++ StudentAttendace/Services/GroupService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Text.Json;
+using StudentAttendace.Models.DbModels;
+
+namespace StudentAttendace.Services;
+
+public class GroupService(HttpClient httpClient)
+{
+    private readonly HttpClient _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+
+    public async Task<IEnumerable<Group>> GetGroupsByTeacherIdAsync(string? teacherId)
+    {
+        try
+        {
+            HttpResponseMessage responseMessage = await _httpClient.GetAsync($"api/Groups/ByTeacher/{teacherId}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string content = await responseMessage.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<IEnumerable<Group>>(content, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                }) ?? throw new InvalidCastException();
+            }
+            else
+            {
+                throw new HttpRequestException($"{responseMessage.StatusCode}");
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+            throw;
+        }
+    }
+}
