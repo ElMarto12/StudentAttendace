f52710f0bcf0a538799d1472c90c15face94fb4e
ElMarto12 <m10danys@gmail.com>
2024-10-15 23:50:41 +0300

    Added Subjects controller to do things with Subject data

---
+++ StudentAttendace/Controllers/API/SubjectsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentAttendace.Config;
+using StudentAttendace.Models.DbModels;
+
+namespace StudentAttendace.Controllers.API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SubjectsController(ApplicationDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Subject>>> GetSubjects()
+    {
+        return await context.Subjects.ToListAsync();
+    }
+    
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<ActionResult<Subject>> GetSubjectById(int id)
+    {
+        var subject = await context.Subjects.FindAsync(id);
+
+        if (subject == null)
+        {
+            return NotFound();
+        }
+
+        return subject;
+    }
+}
