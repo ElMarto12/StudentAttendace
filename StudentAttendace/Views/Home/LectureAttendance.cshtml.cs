3ced83fb509ba6a3051084972dce5ad4d4ba4bf7
ElMarto12 <m10danys@gmail.com>
2024-10-15 23:44:51 +0300

    Added LectureAttendance page, where you can check students attendance

---
+++ StudentAttendace/Views/Home/LectureAttendance.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentAttendace.Views.Home;
+
+public class LectureAttendance : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
