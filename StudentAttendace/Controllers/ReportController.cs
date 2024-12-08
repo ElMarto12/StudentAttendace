5dce76ef964a4406b0a15d24d42dc7331d026f1d
Martynas Danys <m10danys@gmail.com>
2024-12-09 00:08:07 +0200

    Added new Report controller for report's generation

---
+++ StudentAttendace/Controllers/ReportController.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using StudentAttendace.Models.DbModels;
+using StudentAttendace.Services;
+
+namespace StudentAttendace.Controllers;
+
+public class ReportController(PdfReportService pdfService, StudentService studentService, IWebHostEnvironment environment, TeacherService teacherService, SubjectService subjectService, GroupService groupService) : Controller
+{
+    public async Task<IActionResult> GenerateReportBySubject(int subjectId)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var teacher = await teacherService.GetTeacherByUserIdAsync(userId);
+        var subject = await subjectService.GetSubjectByIdAsync(subjectId.ToString());
+        var groups = await groupService.GetGroupsBySubjectIdAsync(subjectId.ToString());
+
+        List<Student> students = new List<Student>();
+
+        foreach (var group in groups)
+        {
+            students.AddRange(await studentService.GetStudentsByGroupIdAsync(group.GroupID.ToString()));
+        }
+        
+        string filePath = Path.Combine(environment.WebRootPath, "Reports", "report.pdf");
+        string fileTitle = "Lankomumas";
+        
+        pdfService.GenerateAttendanceReport(filePath, students, teacher, subject, groups, fileTitle);
+
+        var stream = System.IO.File.OpenRead(filePath);
+        return File(stream, "application/pdf", "report.pdf");
+    }
+}
